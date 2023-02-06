c4bd5c0ce67cbaf9855812725fe76ae4c4f6c8e7
Guy Baruch <guy.baruch@icloud.com>
2023-02-06 19:28:52 +0200

    First dully coded version. Still not working.

--- src/display_multires.cu
+++ src/display_multires.cu
@@ -56,6 +56,8 @@ __host__ __device__ struct Rectangle {
     int stride;
     int height;
     
+    __host__ __device__ Rectangle():
+        x0(0), y0(0), width(0), height(0), stride(0) {}
     __host__ __device__ Rectangle(int x0, int y0, int width, int height, int pitch):
         x0(x0), y0(y0), width(width), height(height), stride(pitch) {}
     __host__ __device__ inline int x_end() {return x0 + width;}
@@ -75,7 +77,7 @@ struct Args {
 Args parse_args(int argc, char** argv) {
     Args args;
     if (1) {
-        args.k = 1;
+        args.k = 2;
         args.input_name = "data/sloth-gray.png";
         args.output_name = "data/sloth-gray-mr.png";
         return args;
@@ -125,6 +127,105 @@ __global__ void init_trg_kernel(const Npp8u* src, Npp8u* trg,
     }
 }
 
+// Take a source subrectangle of the image downscale it by a factor of X2 and output
+// the result to a target subrectangle
+__global__ void downscale_subimage_kernel(Npp8u* img,
+        Rectangle fullRect, Rectangle srcRect, Rectangle trgRect) {
+    const int tidx_x = gridDim.x * blockIdx.x + threadIdx.x;
+    const int tidx_y = gridDim.y * blockIdx.y + threadIdx.y;
+
+    __shared__ Npp8u buff[2][X_STRIDE];
+
+    const int src_x = srcRect.x0 + tidx_x;
+    const int src_y = srcRect.y0 + tidx_y;
+    const int src_addr = src_y * srcRect.stride + src_x;
+
+    if (tidx_x < srcRect.width && src_x < fullRect.width
+            && tidx_y < srcRect.height && src_y < fullRect.height
+            && src_addr < fullRect.numElements()) {
+        buff[threadIdx.y][threadIdx.x] = img[src_addr];
+    } else {
+        buff[threadIdx.y][threadIdx.x] = 0;
+    }
+    __syncthreads();
+
+    if (threadIdx.x%2==0 && threadIdx.y==0) {
+        buff[threadIdx.y][threadIdx.x] =
+            (Npp8u) ( (
+                ((int) buff[threadIdx.y+0][threadIdx.x+0])
+                + ((int) buff[threadIdx.y+0][threadIdx.x+1])
+                + ((int) buff[threadIdx.y+1][threadIdx.x+0])
+                + ((int) buff[threadIdx.y+1][threadIdx.x+1])
+                ) / 4 );
+    }
+    __syncthreads();
+
+    const int trg_x = trgRect.x0 + tidx_x/2;
+    const int trg_y = trgRect.y0 + tidx_y/2;
+    const int trg_addr = trg_y * trgRect.stride + trg_x;
+
+    if (threadIdx.x%2==0 && threadIdx.y==0
+            && tidx_x/2 < trgRect.width && trg_x < fullRect.width
+            && tidx_y/2 < trgRect.height && trg_y < fullRect.height
+            && trg_addr < fullRect.numElements()) {
+        img[trg_addr] = buff[threadIdx.y][threadIdx.x];
+    }
+}
+
+
+// Calculates the next target rectangle, downscales the source subimage to it, and
+// returns the target rectangle.
+__host__ Rectangle downscale_subimage(Npp8u* img_ptr, Rectangle imgRect, Rectangle
+        srcRect, int k) {
+    Rectangle trgRect;
+    // Rectangle(int x0, int y0, int width, int height, int pitch):
+    if (!k) {
+        // First rectangle is to the left of the original image;
+        trgRect = Rectangle(
+                srcRect.x_end(),    // new x0
+                0,                  // new y0
+                srcRect.width/2,    // new width
+                srcRect.height/2,   // new height
+                srcRect.stride);    // same stride.
+    } else {
+        // The next rectangles are placed downwards of each other
+        trgRect = Rectangle(
+                srcRect.x0 + srcRect.width/2,   // new x0
+                srcRect.y_end(),                // new x0
+                srcRect.width/2,                // new width
+                srcRect.height/2,               // new height
+                srcRect.stride);                // same stride.
+    }
+
+    std::cout <<
+        "img ["
+        << imgRect.x0 << ","
+        << imgRect.y0 << ","
+        << imgRect.width << ","
+        << imgRect.height
+        << "]: downscale ["
+        << srcRect.x0 << ","
+        << srcRect.y0 << ","
+        << srcRect.width << ","
+        << srcRect.height
+        << "] -> ["
+        << trgRect.x0 << ","
+        << trgRect.y0 << ","
+        << trgRect.width << ","
+        << trgRect.height << "]" << std::endl;
+
+    dim3 grid( (srcRect.numElements() + X_STRIDE - 1) / X_STRIDE,
+            srcRect.height/2);
+    dim3 tpb(X_STRIDE, 2);
+
+    downscale_subimage_kernel<<<grid, tpb>>>(img_ptr,
+            imgRect, srcRect, trgRect);
+    CUDA_CALL(cudaGetLastError());
+    CUDA_CALL(cudaDeviceSynchronize());
+
+    return trgRect;
+}
+
 int main (int argc, char **argv)
 {
 
@@ -163,11 +264,22 @@ int main (int argc, char **argv)
 
     // The target image has 3/2 
     int trg_width = (src_rect.width*3+1) / 2;
-    npp::ImageCPU_8u_C1 h_trg_C1(trg_width+1, src_rect.height);
-    npp::ImageNPP_8u_C1 d_trg_C1(h_src_C1);
+    npp::ImageCPU_8u_C1 h_trg_C1(trg_width, src_rect.height);
+    npp::ImageNPP_8u_C1 d_trg_C1(h_trg_C1);
 
     Rectangle trg_rect(0, 0, (int)d_trg_C1.width(), (int)d_trg_C1.height(),
             (int)d_trg_C1.pitch());
+    std::cout <<
+        "img ["
+        << src_rect.x0 << ","
+        << src_rect.y0 << ","
+        << src_rect.width << ","
+        << src_rect.height
+        << "] -> ["
+        << trg_rect.x0 << ","
+        << trg_rect.y0 << ","
+        << trg_rect.width << ","
+        << trg_rect.height << "]" << std::endl;
 
     dim3 grid( (trg_rect.numElements() + X_STRIDE - 1) / X_STRIDE,
             trg_rect.height);
@@ -178,6 +290,12 @@ int main (int argc, char **argv)
     
     CUDA_CALL(cudaGetLastError());
     CUDA_CALL(cudaDeviceSynchronize());
+
+    Rectangle currRect = src_rect;
+    for(int k=0; k<args.k; ++k) {
+        currRect = downscale_subimage(
+                d_trg_C1.data(), trg_rect, currRect, k);
+    }
 
     d_trg_C1.copyTo(h_trg_C1.data(), h_trg_C1.pitch());
 
